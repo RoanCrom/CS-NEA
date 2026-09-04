78e0e20c0c3677b3370f50faee09ef5e116d8e12
Roan C <128052665+RoanCrom@users.noreply.github.com>
2026-09-04 15:11:19 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,16 +3,28 @@ import random
 def start () :
 
     question_list =["1","2","3","4","5"]
-    answer_list = ["a","a","a","a","a"]
+    answer_list = ["a","b","c","d","e"]
+    weight_list = [1,1,1,1,1,]
 
-    
-    x = random.randint([0,4])
-    
-    Question = question_list[x]
-    Answer = answer_list[x]
 
-    print(Question)
-    print(Answer)
+    while True:
+            
+        x = random.choices(range(len(question_list)),weights=weight_list)[0]
+        
+        Question = question_list[x]
+        Answer = answer_list[x]
 
+        AskedQ = input(f"{Question}\n")
+
+        if AskedQ.lower() == Answer :
+            print("Correct")
+
+            if weights[x] > 1:
+                weights[x] -= 1
+            else:
+                weights[x] = weights[x]
+        else:
+            print("incorrect")
+            weights[x] += 1
 
 start()
